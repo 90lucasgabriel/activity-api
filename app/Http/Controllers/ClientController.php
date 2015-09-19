7a22cb4bf800fd6536be7643a6101b0db4e90708
Lucas Gabriel <90lucasgabriel@gmail.com>
2015-09-18 23:47:59 -0300

    - Creation Client (Model, Controller, Migration and Seeds) - Creation Client CRUD - Change namespace to CodeProject

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+use CodeProject\Client;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Client::find($id)->delete();
+    }
+}
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,22 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(CodeProject\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(CodeProject\Client::class, function (Faker\Generator $faker) {
+    return [
+        'name' 				=> $faker->name,
+        'responsible' 		=> $faker->name,
+        'email' 			=> $faker->email,
+        'phone' 			=> $faker->phoneNumber,
+        'address' 			=> $faker->address,
+        'obs'				=> $faker->sentence
     ];
 });
