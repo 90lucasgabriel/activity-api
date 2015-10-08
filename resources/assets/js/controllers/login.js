0b27f324faa3a9b6c0c6f059cfc873cdd94a643d
Lucas Gabriel <90lucasgabriel@gmail.com>
2015-10-07 21:59:15 -0300

    - Login	with validation - Showing login errors

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -14,11 +14,18 @@
 			password: ''
 		};
 
+		$scope.error = {
+			message: '',
+			error: false
+		};
+
+
 		$scope.login = function(){
 			OAuth.getAccessToken($scope.user).then(function(){
 				$location.path('home');
-			}, function(){
-				alert('Login Invalid');
+			}, function(data){
+				$scope.error.error = true;
+				$scope.error.message = data.data.error_description;
 			});
 		}
 	};
